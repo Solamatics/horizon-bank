e14eb02fdff400e33d9fe811d3fbe85480aebdc2
Solamatics <afikodeolaitan@gmail.com>
2024-05-17 08:10:14 +0100

    add total balance screen

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Olusola" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "guest"}
+          subtext="Access and manage your transactions efficiently"
+        />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1230.35}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
